5f452988da1c4c804c79d408a54ec3cb967cf25e
Sujal Soni <sujal28soni@gmail.com>
2024-04-28 13:04:00 +0530

    Updated navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-600'>Estate</span>
             <span className='text-slate-900'>Verse</span>
           </h1>
         </Link>
