0624c924b201521f011b82f4f33a6c606f473893
Sujal Soni <112640952+Sujal-2820@users.noreply.github.com>
2024-04-29 18:15:19 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-<<<<<<< HEAD
         target: 'https://real-estate-web-application-8sb7.onrender.com',
-=======
-        target: 'https://real-estate-web-application-8sb7.onrender.com/',
->>>>>>> 2ff31fa6d3957591e5b41bbce10f417266f9d6de
         secure: false,
       },
     },
