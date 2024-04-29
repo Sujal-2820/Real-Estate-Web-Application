bc6254d1f3455f08e220241148d83e1a530012d0
Sujal Soni <sujal28soni@gmail.com>
2024-04-29 18:39:03 +0530

    Updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://real-estate-web-application-8sb7.onrender.com',
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://real-estate-web-application-8sb7.onrender.com',
+  //       secure: false,
+  //     },
+  //   },
+  // },
 
   plugins: [react()],
 });
